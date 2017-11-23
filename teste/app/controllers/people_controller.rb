b8caee9f4a6b59411e82b3cdd5c3fbea232bc878
vagrant <vagrant@vagrant.vm>
2017-11-23 18:23:28 +0000

    Aula 23/11 - teste Rais e agenda usando Scaffold

---
+++ teste/app/controllers/people_controller.rb
@@ -0,0 +1,7 @@
+class PeopleController < ApplicationController
+  #action
+  def index
+  	@nome= "José Ricardo"
+  	@numeros = [1,2,3,4]
+  end
+end
---
+++ teste/app/helpers/people_helper.rb
@@ -0,0 +1,5 @@
+module PeopleHelper
+	def nome_completo
+		"José Ricardo Bettini Pacola"
+	end
+end
